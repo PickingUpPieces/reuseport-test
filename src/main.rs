3c35c823689b6aae7037d3c6b6b048bb14f61bd7
Vincent Picking <v.picking@gmx.de>
2024-02-23 13:30:20 +0100

    Print out sending and receiving port

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn server() {
             loop {
                 let mut buf = [0u8; 1024];
                 let len = recv(socket.as_raw_fd(), &mut buf, MsgFlags::empty()).expect("Failed to receive message");
-                info!("Received message on THREAD {}: {:?}", i, std::str::from_utf8(&buf[..len]).unwrap());
+                info!("Received on THREAD {}, port {}: {:?}", i, PORT, std::str::from_utf8(&buf[..len]).unwrap());
             }
         }));
     }
@@ -59,8 +59,11 @@ fn client() {
             let addr = SockaddrIn::from_str(&addr).expect("Failed to parse socketaddr");
             connect(socket.as_raw_fd(), &addr).expect("Connecting to server failed");
 
+            let sockaddr: SockaddrIn = getsockname(socket.as_raw_fd()).expect("Failed to get socket address");
+            let port = sockaddr.port();
+
             for _ in 0..10 {
-                let msg = format!("Hello from THREAD {} !", i);
+                let msg = format!("THREAD {}: port {}", i, port);
                 send(socket.as_raw_fd(), msg.as_bytes(), MsgFlags::empty()).expect("Failed to send message");
                 info!("Sent message: {}", msg);
             }
