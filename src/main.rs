1213d110f98bfaeccb2f8e53b08ac2bee3f32703
Vincent Picking <v.picking@gmx.de>
2024-02-23 14:36:10 +0100

    Count messages from each thread

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::{os::fd::AsRawFd, str::FromStr, thread::JoinHandle};
+use std::{collections::HashMap, os::fd::AsRawFd, str::FromStr, thread::{self, JoinHandle}};
 use nix::sys::socket::{sockopt::ReusePort, *};
 use clap::Parser;
 use log::info;
+use serde::{Deserialize, Serialize};
 
 const PORT: u16 = 45103;
 const SERVER_THREADS: usize = 5;
-const CLIENT_THREADS: usize = 2;
+const CLIENT_THREADS: usize = 10;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+    thread: usize,
+    port: u16,
+}
 
 #[derive(Debug, Parser, Default)]
 #[clap(name = "reuseport")]
@@ -38,11 +45,15 @@ fn server() {
             setsockopt(&socket, ReusePort, &true).expect("Setting SO_REUSEPORT failed");
             bind(socket.as_raw_fd(), &addr).expect("Failed to bind listener");
 
+            let mut map: HashMap<u16, usize>= HashMap::new();
             // Receive in a loop
             loop {
                 let mut buf = [0u8; 1024];
                 let len = recv(socket.as_raw_fd(), &mut buf, MsgFlags::empty()).expect("Failed to receive message");
-                info!("Received on THREAD {}, port {}: {:?}", i, PORT, std::str::from_utf8(&buf[..len]).unwrap());
+                let msg: Message = serde_json::from_str(std::str::from_utf8(&buf[..len]).unwrap()).unwrap();
+
+                map.entry(msg.port).and_modify(|count| *count += 1).or_insert(1);
+                info!("THREAD {}: {:?}", i, map);
             }
         }));
     }
@@ -62,10 +73,13 @@ fn client() {
             let sockaddr: SockaddrIn = getsockname(socket.as_raw_fd()).expect("Failed to get socket address");
             let port = sockaddr.port();
 
-            for _ in 0..10 {
-                let msg = format!("THREAD {}: port {}", i, port);
-                send(socket.as_raw_fd(), msg.as_bytes(), MsgFlags::empty()).expect("Failed to send message");
-                info!("Sent message: {}", msg);
+            let msg = Message { thread: i, port };
+
+            for _ in 0..100 {
+                let msg_json = serde_json::to_string(&msg).unwrap();
+                send(socket.as_raw_fd(), msg_json.as_bytes(), MsgFlags::empty()).expect("Failed to send message");
+                info!("Sent message: {}", msg_json);
+                thread::sleep(std::time::Duration::from_millis(10));
             }
         }));
     }
